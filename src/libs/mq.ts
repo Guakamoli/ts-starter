fe8e2c9228d5bcbc131c51fa4af094eebb737c84
褪色仙人 <zzz@alloc.cloud>
2022-05-23 16:06:04 +0800

    chore: 支持多个 topic

--- src/libs/mq.ts
+++ src/libs/mq.ts
@@ -1,28 +1,39 @@
 import { MQClient, MessageProperties } from '@aliyunmq/mq-http-sdk';
-import { MQOptions } from '../types';
+import { MQOptions, Topic } from '../types';
 
 export class MQ {
   instanceId: string;
-  topicId: string;
+  topics: Array<Topic>;
 
   client: any;
-  consumer: any;
+  consumers = new Map();
 
   constructor(options: MQOptions) {
-    const {
-      accessKeyId,
-      accessKeySecret,
-      endpoint,
-      instanceId,
-      topicId,
-      groupId,
-    } = options;
+    const { accessKeyId, accessKeySecret, endpoint, instanceId, topics } =
+      options;
 
     this.instanceId = instanceId;
-    this.topicId = topicId;
+    this.topics = topics;
 
     this.client = new MQClient(endpoint, accessKeyId, accessKeySecret);
-    this.consumer = this.client.getConsumer(instanceId, topicId, groupId);
+  }
+
+  getConsumer(topicId: string) {
+    if (this.consumers.has(topicId)) {
+      return this.consumers.get(topicId);
+    } else {
+      const topic = this.topics.find((topic) => topic.topicId === topicId);
+      if (!topic) {
+        throw new Error(`Topic not found. topicId: ${topicId}`);
+      }
+      const consumer = this.client.getConsumer(
+        this.instanceId,
+        topic.topicId,
+        topic.groupId,
+      );
+      this.consumers.set(topicId, consumer);
+      return consumer;
+    }
   }
 
   /**
@@ -46,14 +57,22 @@ export class MQ {
 
   /**
    * @description 发送RockerMQ消息
-   * @param {string} body 消息体
+   * @param {string} topic 主题ID
+   * @param {Object} body 消息体
    * @param {string} tag 消息标签
    * @param {string} messageKey 消息key
    * @param {Object} props 消息属性
    * @param {number} retry 消息重试次数
    * @returns {Promise<void>} result
    */
-  async send(body: string, tag: string, messageKey = '', props = {}, retry = 3) {
+  async send(
+    topic: string,
+    body: any,
+    tag: string,
+    messageKey = '',
+    props = {},
+    retry = 3,
+  ) {
     if (!body) {
       return;
     }
@@ -64,14 +83,14 @@ export class MQ {
     }
 
     try {
-      const producer = this.client.getProducer(this.instanceId, this.topicId);
+      const producer = this.client.getProducer(this.instanceId, topic);
       await producer.publishMessage(body, tag, msgProps);
       console.log('send message success');
     } catch (error) {
       console.log('send message error', error);
       if (retry > 0) {
         // 消息发送失败，需要进行重试处理，可重新发送这条消息或持久化这条数据进行补偿处理。
-        await this.send(body, tag, messageKey, props, retry - 1);
+        await this.send(topic, body, tag, messageKey, props, retry - 1);
       }
     }
   }
